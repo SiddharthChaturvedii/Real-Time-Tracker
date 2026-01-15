7f8682e71663df61dc1e3cb4d553f9fefd03a0ed
SiddharthChaturvedii <chaturvedisiddharth008@gmail.com>
2026-01-15 17:18:44 +0530

    trying to fix the LeaveParty button

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const http = require("http");
 const { v4: uuidv4 } = require("uuid");
 
 const server = http.createServer(app);
-let partySOS = {}; // partyCode -> socket.id
-
 
 const io = require("socket.io")(server, {
   cors: {
@@ -17,9 +15,38 @@ const io = require("socket.io")(server, {
 // =============================
 // STATE
 // =============================
-const users = {};
-const parties = {};
-const userParty = {};
+const users = {};        // socket.id -> username
+const parties = {};      // partyCode -> [{ id, username }]
+const userParty = {};    // socket.id -> partyCode
+const partySOS = {};     // partyCode -> socket.id
+
+// =============================
+// HELPERS
+// =============================
+function removeUserFromParty(socket) {
+  const code = userParty[socket.id];
+  if (!code || !parties[code]) return;
+
+  // remove user
+  parties[code] = parties[code].filter(u => u.id !== socket.id);
+  socket.leave(code);
+  delete userParty[socket.id];
+
+  // clear SOS if that user was the one
+  if (partySOS[code] === socket.id) {
+    delete partySOS[code];
+    io.to(code).emit("sosUpdate", { userId: null });
+  }
+
+  // notify party
+  io.to(code).emit("user-disconnected", socket.id);
+
+  // delete party if empty
+  if (parties[code].length === 0) {
+    delete parties[code];
+    delete partySOS[code];
+  }
+}
 
 // =============================
 // SOCKET
@@ -45,8 +72,6 @@ io.on("connection", (socket) => {
     });
   });
 
-
-
   socket.on("joinParty", ({ partyCode, username }) => {
     if (!parties[partyCode]) {
       socket.emit("partyError", "Party does not exist");
@@ -68,22 +93,8 @@ io.on("connection", (socket) => {
   });
 
   socket.on("leaveParty", () => {
-  const code = userParty[socket.id];
-  if (!code) return;
-
-  parties[code] = parties[code].filter(u => u.id !== socket.id);
-
-  socket.leave(code);
-  delete userParty[socket.id];
-
-  io.to(code).emit("user-disconnected", socket.id);
-
-  if (parties[code].length === 0) {
-    delete parties[code];
-  }
-});
-
-
+    removeUserFromParty(socket);
+  });
 
   socket.on("send-location", (data) => {
     const code = userParty[socket.id];
@@ -97,59 +108,36 @@ io.on("connection", (socket) => {
     });
   });
 
-  socket.on("disconnect", () => {
-    const code = userParty[socket.id];
-    if (code && parties[code]) {
-      parties[code] = parties[code].filter(
-        (u) => u.id !== socket.id
-      );
-      io.to(code).emit("user-disconnected", socket.id);
-      if (!parties[code].length) delete parties[code];
-
-      if (partySOS[code] === socket.id) {
-        delete partySOS[code];
-        io.to(code).emit("sosUpdate", { userId: null });
-}
-
-    }
-    delete users[socket.id];
-    delete userParty[socket.id];
-  });
   // =============================
-// SOS SYSTEM
-// =============================
-    socket.on("sos", () => {
-      const code = userParty[socket.id];
-      if (!code) return;
+  // SOS SYSTEM (single source of truth)
+  // =============================
+  socket.on("sos", () => {
+    const code = userParty[socket.id];
+    if (!code) return;
 
-      io.to(code).emit("sos-alert", {
-        id: socket.id,
-        username: users[socket.id],
-        time: Date.now()
-      });
+    partySOS[code] = socket.id;
+
+    io.to(code).emit("sosUpdate", {
+      userId: socket.id,
+      username: users[socket.id],
+      time: Date.now(),
     });
-
-    socket.on("sos-clear", () => {
-      const code = userParty[socket.id];
-      if (!code) return;
-
-      io.to(code).emit("sos-clear", socket.id);
-    });
-
-    socket.on("sos", () => {
-  const code = userParty[socket.id];
-  if (!code) return;
-
-  partySOS[code] = socket.id;
-
-  io.to(code).emit("sosUpdate", {
-    userId: socket.id,
-    username: users[socket.id],
   });
-});
 
+  socket.on("sos-clear", () => {
+    const code = userParty[socket.id];
+    if (!code) return;
 
+    if (partySOS[code] === socket.id) {
+      delete partySOS[code];
+      io.to(code).emit("sosUpdate", { userId: null });
+    }
+  });
 
+  socket.on("disconnect", () => {
+    removeUserFromParty(socket);
+    delete users[socket.id];
+  });
 });
 
 server.listen(3000, () =>
