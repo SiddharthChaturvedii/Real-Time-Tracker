f10b278a3b0e77925858b5fce10fb01887e5594f
SiddharthChaturvedii <chaturvedisiddharth008@gmail.com>
2026-01-19 14:34:04 +0530

    Backend Deployed!!

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -1,7 +1,7 @@
 "use client";
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://real-time-tracker-0qge.onrender.com", {
   transports: ["websocket"],
   autoConnect: true,
 });
